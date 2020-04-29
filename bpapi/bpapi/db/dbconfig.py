24139ed92a4be1440c3c959d6855f86f3b0073cc
Waldir Borba Junior <wborbajr@gmail.com>
2020-04-29 19:08:20 -0300

    feat: fixed database address

---
+++ bpapi/bpapi/db/dbconfig.py
@@ -0,0 +1,26 @@
+from sqlalchemy import (Column, Integer, MetaData, String, Table, DateTime,
+                        create_engine, ARRAY)
+from sqlalchemy import func
+from databases import Database
+
+# SQLAlchemy specific code, as with any other app
+DATABASE_URL = "sqlite:///./bptogether.db"
+# DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+# SQLAlchemy
+# engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+metadata = MetaData()
+notes = Table (
+    "notes",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String(50)),
+    Column("description", String(50)),
+    Column("created_date", DateTime, default=func.now(), nullable=False),
+)
+
+# databases query builder
+database = Database(DATABASE_URL)
--- bpapi/bpapi/main.py
+++ bpapi/bpapi/main.py
@@ -1,44 +1,14 @@
 from typing import List
-import databases
-import sqlalchemy
-from pydantic import BaseModel
-from routers import ping
+
 from fastapi import Depends, FastAPI, Header, HTTPException
 
+# importing database connection
+from db.dbconfig import database, engine, metadata
 
+# importing routes
+from routers import ping
 
-# SQLAlchemy specific code, as with any other app
-DATABASE_URL = "sqlite:///./bptogether.db"
-# DATABASE_URL = "postgresql://user:password@postgresserver/db"
-
-database = databases.Database(DATABASE_URL)
-
-metadata = sqlalchemy.MetaData()
-
-notes = sqlalchemy.Table(
-    "notes",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("text", sqlalchemy.String),
-    sqlalchemy.Column("completed", sqlalchemy.Boolean),
-)
-
-
-engine = sqlalchemy.create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
 metadata.create_all(engine)
-
-
-class NoteIn(BaseModel):
-    text: str
-    completed: bool
-
-
-class Note(BaseModel):
-    id: int
-    text: str
-    completed: bool
 
 
 app = FastAPI()
@@ -54,31 +24,4 @@ async def shutdown():
     await database.disconnect()
 
 
-# @app.get("/notes/", response_model=List[Note])
-# async def read_notes():
-#     query = notes.select()
-#     return await database.fetch_all(query)
-#
-#
-# @app.post("/notes/", response_model=Note)
-# async def create_note(note: NoteIn):
-#     query = notes.insert().values(text=note.text, completed=note.completed)
-#     last_record_id = await database.execute(query)
-#     return {**note.dict(), "id": last_record_id}
-#
-
-
 app.include_router(ping.router)
-
-# async def get_token_header(x_token: str = Header(...)):
-#     if x_token != "fake-super-secret-token":
-#         raise HTTPException(status_code=400, detail="X-Token header invalid")
-
-# app.include_router(users.router)
-# app.include_router(
-#     items.router,
-#     prefix="/items",
-#     tags=["items"],
-#     dependencies=[Depends(get_token_header)],
-#     responses={404: {"description": "Not found"}},
-# )
