2c915be1b2349dabd1b5a9f4deb7552bf4259fdf
Waldir Borba Junior <wborbajr@gmail.com>
2020-04-29 19:13:55 -0300

    feat: log added

--- bpapi/bpapi/main.py
+++ bpapi/bpapi/main.py
@@ -8,6 +8,13 @@ from db.dbconfig import database, engine, metadata
 # importing routes
 from routers import ping
 
+from loguru import logger
+
+# log
+logger.add(
+    "file.log", format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}"
+)
+
 metadata.create_all(engine)
 
 
@@ -16,11 +23,13 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
+    logger.info("Database Startup")
     await database.connect()
 
 
 @app.on_event("shutdown")
 async def shutdown():
+    logger.info("Database Shutdown")
     await database.disconnect()
 
 
